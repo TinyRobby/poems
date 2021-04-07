3612310b44b80923d5d7e53fba51043bdc19db06
Robby <>
2021-04-07 18:43:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#function area
+def dimante():
+    print('dimante')
+    poet=input('noun: ')
+    nxt=input('2 agectives(relating to first noun): ')
+    aftr=input('3 verbs(relating to first noun): ')
+
+    def write2filedim():
+        filewrt=open('poem.txt', 'x')
+        filewrt.write(poet, '\n', nxt, '\n', aftr, otr)
+#ordinary code area
+print('poem maker')
